f5dbd8872878127bc29c30f941e14ede992c7794
Daria <bondarika@yandex.ru>
2024-12-06 21:38:58 +0300

    removed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
-
+import { Header } from "./components"
 export default function App() {
-    return (
-        <>
-
-        </>
-    );
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+﻿import { logo } from "../constants/images";
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+    </header>
+  );
+};
+
+export default Header;
