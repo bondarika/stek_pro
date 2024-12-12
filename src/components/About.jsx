df45104630928a4c4dbb7ec5c5651ff98f8b3167
bondarika <bondarika1@gmail.com>
2024-12-13 02:28:49 +0300

    swiper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
-﻿
+﻿import Slider from "./Swiper";
 const About = () => {
   return (
     <>
       <div className="container">
         <div className="about">
           <p>О ПРОЕКТЕ</p>
+          <Slider />
         </div>
       </div>
     </>
--- src/constants/images.js
+++ src/constants/images.js
@@ -2,11 +2,14 @@ import logo from "@public/images/logo.svg";
 import child from "@public/images/child.png";
 import video from "@public/images/video.png";
 import h1 from "@public/images/h1.png";
-
+import swiper_1 from "@public/images/swiper_1.png";
+import swiper_2 from "@public/images/swiper_2.png";
 
 export {
     logo,
     child,
     video,
-    h1
+    h1,
+    swiper_1,
+    swiper_2
 };
