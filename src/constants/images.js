735ab5daf89ab68b1e8217efbeee6290148d950a
Daria <bondarika@yandex.ru>
2025-01-04 16:03:24 +0300

    .

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,6 @@
 import logo from "@public/images/logo.svg";
 import child from "@public/images/child.png";
 import video from "@public/images/video.png";
-import h1 from "@public/images/h1.png";
 import swiper_1 from "@public/images/swiper_1.png";
 import swiper_2 from "@public/images/swiper_2.png";
 import arrow_left from "@public/images/arrow_left.svg";
@@ -38,7 +37,6 @@ export {
   logo,
   child,
   video,
-  h1,
   swiper_1,
   swiper_2,
   arrow_left,
