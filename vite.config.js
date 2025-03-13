abaa10b4b5c5ef5907e6dceaef847b5128a136cc
bondarika <bondarika@yandex.ru>
2025-03-13 10:22:19 +0300

    set alias move images

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "src"),
       "@public": path.resolve(__dirname, "./public"),
     },
   },
